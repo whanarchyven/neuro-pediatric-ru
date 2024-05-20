3368d71033da4b91a046727f50b076c6c9a0ae64
Ivan Zaburdaev <ivanlom227@gmail.com>
2024-05-20 14:07:35 +0300

    try to pn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,8 +8,8 @@ import TreatmentStage, {
 import { getAnamnes } from '@/shared/utils/getAnamnes';
 import { useEffect, useState } from 'react';
 import { useSearchParams } from 'next/navigation';
-// import LoadingIcon from '../../loading.svg';
-import { usePDF } from 'react-to-pdf';
+import LoadingIcon from '../../loading.svg';
+import generatePDF, { Resolution, Options } from 'react-to-pdf';
 
 export default function Home() {
   const searchParams = useSearchParams();
@@ -66,15 +66,45 @@ export default function Home() {
     treatmentStages: [],
   };
 
-  const { toPDF, targetRef } = usePDF({ filename: 'page.pdf' });
+  const options: Options = {
+    filename: 'advanced-example.pdf',
+    // default is `save`
+    method: 'save',
+    // default is Resolution.MEDIUM = 3, which should be enough, higher values
+    // increases the image quality but also the size of the PDF, so be careful
+    // using values higher than 10 when having multiple pages generated, it
+    // might cause the page to crash or hang.
+    resolution: Resolution.NORMAL,
+    canvas: {
+      // default is 'image/jpeg' for better size performance
+      mimeType: 'image/png',
+      qualityRatio: 1,
+    },
+    // customize any value passed to the jsPDF instance and html2canvas
+    // function
+    overrides: {
+      // see https://artskydj.github.io/jsPDF/docs/jsPDF.html for more options
+      pdf: {
+        compress: true,
+      },
+      // see https://html2canvas.hertzen.com/configuration for more options
+      canvas: {
+        useCORS: true,
+      },
+    },
+  };
+
+  const openPDF = () => {
+    generatePDF(() => document.getElementById('wrapper'), options);
+  };
 
   return (
-    <div className={'bg-wrapper pb-10'} ref={targetRef}>
+    <div className={'bg-wrapper pb-10'} id={'wrapper'}>
       {!isLoading && (
         <div
           style={{ transform: 'translate(-50%, -50%)' }}
           onClick={() => {
-            toPDF();
+            openPDF();
           }}
           className={
             'font-bold min-w-fit cursor-pointer text-sm p-2 rounded-full whitespace-nowrap text-black bg-white fixed z-[99999999] right-[50%] left-[50%] md:left-auto bottom-0.5 md:bottom-2 md:right-2 flex items-center justify-center'
@@ -83,14 +113,14 @@ export default function Home() {
         </div>
       )}
       <main className={'flex flex-col gap-2'}>
-        {/*{isLoading && (*/}
-        {/*  <div*/}
-        {/*    className={*/}
-        {/*      'fixed top-0 left-0 loading z-50 w-full h-full flex justify-center items-center'*/}
-        {/*    }>*/}
-        {/*    <LoadingIcon className={'w-[50px] aspect-square animate-spin'} />*/}
-        {/*  </div>*/}
-        {/*)}*/}
+        {isLoading && (
+          <div
+            className={
+              'fixed top-0 left-0 loading z-50 w-full h-full flex justify-center items-center'
+            }>
+            <LoadingIcon className={'w-[50px] aspect-square animate-spin'} />
+          </div>
+        )}
 
         <MainBlock
           description={data.mainBlock.description}
